be369fb8a4fcb5fa714160a530f05da06004ae9e
aihassan1 <abdosruor57@gmail.com>
2024-08-19 10:50:42 +0300

    5

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { env } from 'process';
 import routes from './routes/index';
+import bodyParser from 'body-parser'; // Import body-parser
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 const port = env.PORT || 5000;
 
